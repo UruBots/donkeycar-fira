f0790a4f4d44cb54013073274b302fc4431ce7da
Sebastian Barcelona <sbarcelona@gmail.com>
2025-04-01 15:54:49 -0300

    testing detections

--- donkeycar/parts/fira_engine_yolo/fira_engine_yolo.py
+++ donkeycar/parts/fira_engine_yolo/fira_engine_yolo.py
@@ -234,8 +234,8 @@ class FIRAEngineYolo(object):
 
                     if(self.debug_visuals):
                         cv2.putText(img, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                        #cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
-                    cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
                     distance = self.estimate_distance(KNOWN_WIDTH, FOCAL_LENGTH, object_width_px)
 
                     if self.debug_visuals:
